60cb7d43819a876c024d6f252e20ec52c33ad232
olga <o.sopilniak@gmail.com>
2016-05-26 20:23:11 +0300

    Update striped hash set

--- task4/striped_hash_set/striped_hash_set.h
+++ task4/striped_hash_set/striped_hash_set.h
@@ -2,6 +2,8 @@
 #include <vector>
 #include <forward_list>
 #include <shared_mutex>
+#include <atomic>
+#include <iostream>
 
 const int DEFAULT_GROWTH_FACTOR = 2;
 const float DEFAULT_LOAD_FACTOR = 1.5;
@@ -9,7 +11,7 @@ const float DEFAULT_LOAD_FACTOR = 1.5;
 template <typename T, class H = std::hash<T>>
 class striped_hash_set {
 public:
-    explicit striped_hash_set(size_t num_stripes,
+    explicit striped_hash_set(std::size_t num_stripes,
                               int growth_factor = DEFAULT_GROWTH_FACTOR,
                               float load_factor = DEFAULT_LOAD_FACTOR);
     striped_hash_set(striped_hash_set&) = delete;
@@ -25,49 +27,52 @@ public:
 private:
     std::vector<std::forward_list<T>> hash_table_;
     std::vector<std::shared_timed_mutex> mutex_array_;
-
     float load_factor_;
     int growth_factor_;
-    int num_elements_;
+    std::atomic<int> num_elements_;
+    std::atomic<std::size_t> hash_table_size_;
 
     H hash_function_;
     void rehash();
 };
 
 template <typename T, class H>
-striped_hash_set<T, H>::striped_hash_set(size_t num_stripes,
+striped_hash_set<T, H>::striped_hash_set(std::size_t num_stripes,
                                    int growth_factor,
                                    float load_factor)
-        : growth_factor_(growth_factor),
-          load_factor_(load_factor),
-          num_elements_(0),
+        : hash_table_(num_stripes * 10),
           mutex_array_(num_stripes),
-          hash_table_(num_stripes) {
+          load_factor_(load_factor),
+          growth_factor_(growth_factor),
+          num_elements_(0),
+          hash_table_size_(num_stripes * 10) {
 }
 
 template <typename T, class H>
 void striped_hash_set<T, H>::add(const T& element) {
-    int hash = hash_function_(element);
-    if (num_elements_ / hash_table_.size() < load_factor_) {
-        std::unique_lock<std::shared_timed_mutex> lock(
-                mutex_array_[hash % mutex_array_.size()]);
-        for (auto current : hash_table_[hash % hash_table_.size()]) {
-            if (current == element) {
-                return;
-            }
-        }
-        hash_table_[hash % hash_table_.size()].push_front(element);
-        ++num_elements_;
-        return;
-    }
-
     std::vector<std::unique_lock<std::shared_timed_mutex>> lock_array;
     // lock the first mutex
     lock_array.emplace_back(mutex_array_[0]);
 
+    int hash = hash_function_(element);
+    if (num_elements_.load() / hash_table_size_.load() < load_factor_) {
+        mutex_array_[0].unlock();
+        std::unique_lock<std::shared_timed_mutex> lock(
+                mutex_array_[hash % mutex_array_.size()]);
+        for (auto current : hash_table_[hash % hash_table_size_.load()]) {
+            if (current == element) {
+                return;
+            }
+        }
+        hash_table_[hash % hash_table_size_.load()].push_front(element);
+        num_elements_.store(num_elements_ + 1);
+        return;
+    }
+
     // check load factor once again to prevent from data race
-    std::size_t hash_table_size = hash_table_.size();
-    if (hash_table_.size() != hash_table_size) {
+    std::atomic<std::size_t> hash_table_size;
+    hash_table_size.store(hash_table_size_.load());
+    if (hash_table_.size() != hash_table_size.load()) {
         return;
     }
 
@@ -76,18 +81,27 @@ void striped_hash_set<T, H>::add(const T& element) {
         lock_array.emplace_back(mutex_array_[i]);
     }
     rehash();
+
+    for (auto current : hash_table_[hash % hash_table_size_.load()]) {
+        if (current == element) {
+            return;
+        }
+    }
+    hash_table_[hash % hash_table_size_.load()].push_front(element);
+    num_elements_.store(num_elements_ + 1);
 }
 
 template <typename T, class H>
 void striped_hash_set<T, H>::rehash() {
-    std::vector<std::forward_list<T>> new_hash_table;
-    new_hash_table.resize(hash_table_.size() * growth_factor_);
+    std::vector<std::forward_list<T>> new_hash_table(
+            hash_table_size_.load() * growth_factor_);
     for (std::forward_list<T> bucket : hash_table_) {
         for (auto current : bucket) {
-            int hash = hash_function_(current) % new_hash_table.size();
-            new_hash_table[hash].push_front(current);
+            int hash = hash_function_(current);
+            new_hash_table[hash % new_hash_table.size()].push_front(current);
         }
     }
+    hash_table_size_.store(new_hash_table.size());
     hash_table_ = std::move(new_hash_table);
 }
 
@@ -96,9 +110,9 @@ void striped_hash_set<T, H>::remove(const T& element) {
     int hash = hash_function_(element);
     std::unique_lock<std::shared_timed_mutex> lock(
             mutex_array_[hash % mutex_array_.size()]);
-    for (auto current : hash_table_[hash % hash_table_.size()]) {
+    for (auto current : hash_table_[hash % hash_table_size_.load()]) {
         if (current == element) {
-            hash_table_[hash % hash_table_.size()].remove(element);
+            hash_table_[hash % hash_table_size_.load()].remove(element);
         }
     }
 }
@@ -108,7 +122,7 @@ bool striped_hash_set<T, H>::contains(const T& element) {
     int hash = hash_function_(element);
     std::unique_lock<std::shared_timed_mutex> lock(
             mutex_array_[hash % mutex_array_.size()]);
-    for (auto current : hash_table_[hash % hash_table_.size()]) {
+    for (auto current : hash_table_[hash % hash_table_size_.load()]) {
         if (current == element) {
             return true;
         }
